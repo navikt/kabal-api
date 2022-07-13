b466e4a6e2e94188860244287824748c4d541159
Andreas Jonsson <andreas.jonsson@nav.no>
2022-07-13 15:05:48 +0200

    Try to ferdigstill documents "directly but async"

--- src/main/kotlin/no/nav/klage/dokument/service/FerdigstillDokumentService.kt
+++ src/main/kotlin/no/nav/klage/dokument/service/FerdigstillDokumentService.kt
@@ -3,12 +3,17 @@ package no.nav.klage.dokument.service
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
 import no.nav.klage.dokument.domain.Event
+import no.nav.klage.dokument.domain.dokumenterunderarbeid.DokumentUnderArbeid
 import no.nav.klage.dokument.repositories.DokumentUnderArbeidRepository
+import no.nav.klage.oppgave.domain.events.DokumentFerdigstiltAvSaksbehandler
 import no.nav.klage.oppgave.util.getLogger
 import no.nav.klage.oppgave.util.getSecureLogger
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.scheduling.annotation.Async
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import org.springframework.transaction.event.TransactionPhase
+import org.springframework.transaction.event.TransactionalEventListener
 
 @Service
 class FerdigstillDokumentService(
@@ -28,38 +33,48 @@ class FerdigstillDokumentService(
         val hovedDokumenterIkkeFerdigstilte =
             dokumentUnderArbeidRepository.findByMarkertFerdigNotNullAndFerdigstiltNullAndParentIdIsNull()
         for (it in hovedDokumenterIkkeFerdigstilte) {
-            try {
-                if (it.dokumentEnhetId == null) {
-                    dokumentUnderArbeidService.opprettDokumentEnhet(it.id)
-                }
-                dokumentUnderArbeidService.ferdigstillDokumentEnhet(it.id)
+            ferdigstill(it)
+        }
+    }
 
-                //Send to all subscribers. If this fails, it's not the end of the world.
-                runCatching {
-                    val event = Event(
-                        behandlingId = it.behandlingId.toString(),
-                        name = "finished",
-                        id = it.id.id.toString(),
-                        data = it.id.id.toString(),
-                    )
-                    logger.debug("Publishing document event to Kafka for subscribers: {}", event)
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    fun listenToFerdigstilteDokumenterAvSaksbehandler(dokumentFerdigstiltAvSaksbehandler: DokumentFerdigstiltAvSaksbehandler) {
+        logger.debug("listenToFerdigstilteDokumenterAvSaksbehandler called")
+        ferdigstill(dokumentFerdigstiltAvSaksbehandler.dokumentUnderArbeid)
+    }
 
-                    val result = aivenKafkaTemplate.send(
-                        "klage.internal-events.v1",
-                        jacksonObjectMapper().writeValueAsString(event)
-                    ).get()
-                    logger.debug("Published document event to Kafka for subscribers: {}", result)
-                }.onFailure {
-                    logger.error("Could not publish document event to subscribers", it)
-                }
-
-            } catch (e: Exception) {
-                logger.error("Could not 'ferdigstillHovedDokumenter' with dokumentEnhetId: ${it.dokumentEnhetId}. See secure logs.")
-                secureLogger.error(
-                    "Could not 'ferdigstillHovedDokumenter' with dokumentEnhetId: ${it.dokumentEnhetId}",
-                    e
-                )
+    private fun ferdigstill(it: DokumentUnderArbeid) {
+        try {
+            if (it.dokumentEnhetId == null) {
+                dokumentUnderArbeidService.opprettDokumentEnhet(it.id)
             }
+            dokumentUnderArbeidService.ferdigstillDokumentEnhet(it.id)
+
+            //Send to all subscribers. If this fails, it's not the end of the world.
+            runCatching {
+                val event = Event(
+                    behandlingId = it.behandlingId.toString(),
+                    name = "finished",
+                    id = it.id.id.toString(),
+                    data = it.id.id.toString(),
+                )
+                logger.debug("Publishing document event to Kafka for subscribers: {}", event)
+
+                val result = aivenKafkaTemplate.send(
+                    "klage.internal-events.v1",
+                    jacksonObjectMapper().writeValueAsString(event)
+                ).get()
+                logger.debug("Published document event to Kafka for subscribers: {}", result)
+            }.onFailure {
+                logger.error("Could not publish document event to subscribers", it)
+            }
+        } catch (e: Exception) {
+            logger.error("Could not 'ferdigstillHovedDokumenter' with dokumentEnhetId: ${it.dokumentEnhetId}. See secure logs.")
+            secureLogger.error(
+                "Could not 'ferdigstillHovedDokumenter' with dokumentEnhetId: ${it.dokumentEnhetId}",
+                e
+            )
         }
     }
 }
