5aa3bbfaed85bc4c764eff0548ae4f6906886103
Andreas Jonsson <andreas.jonsson@nav.no>
2023-12-08 14:38:56 +0100

    Clear history and start from scratch.
    Co-authored-by: Øyvind Nordsted Wedøe <oyvind.norsted.wedoe@nav.no>

---
+++ src/main/resources/db/migration/V103__delete_history.sql
@@ -0,0 +1,6 @@
+delete from klage.satt_paa_vent_historikk;
+delete from klage.fullmektighistorikk;
+delete from klage.klagerhistorikk;
+delete from klage.medunderskriverhistorikk;
+delete from klage.tildelinghistorikk;
+delete from klage.rolhistorikk;
