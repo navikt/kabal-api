1ea66caf0133817b6ffb7c448a77b20d33c2c565
Andreas Jonsson <andreas.jonsson@nav.no>
2024-01-22 11:46:08 +0100

    Removed unnecessary property.

--- src/main/kotlin/no/nav/klage/dokument/api/view/SmartDocumentView.kt
+++ src/main/kotlin/no/nav/klage/dokument/api/view/SmartDocumentView.kt
@@ -1,10 +1,8 @@
 package no.nav.klage.dokument.api.view
 
 import java.time.LocalDateTime
-import java.util.*
 
 data class SmartDocumentVersionView(
-    val id: UUID,
     val version: Int,
     val author: Author?,
     val timestamp: LocalDateTime,
