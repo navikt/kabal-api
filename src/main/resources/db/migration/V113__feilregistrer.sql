80ffea416b3ef605bc84efd5abb3dd6381ea20ef
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-03-04 15:21:28 +0100

    Set feilregistrert in db.

---
+++ src/main/resources/db/migration/V113__feilregistrer.sql
@@ -0,0 +1,9 @@
+UPDATE klage.behandling d
+SET feilregistrering_registered   = '2023-03-04 14:56:13.000000',
+    feilregistrering_nav_ident    = 'SYSTEM',
+    feilregistrering_reason       = 'Ble ferdigbehandlet utenfor Kabal',
+    feilregistrering_fagsystem_id = '23'
+WHERE id in (
+             '6403e2d1-d88e-4e57-841c-0be0a00c398e',
+             '70ad552f-8f37-4905-8acb-777cf45d9fc9'
+    );
