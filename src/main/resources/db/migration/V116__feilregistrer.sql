663d0d55f0b5517dbab16629125db46cdeababda
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-03-08 12:51:48 +0100

    Set feilregistrert in db after duplicate error.

---
+++ src/main/resources/db/migration/V116__feilregistrer.sql
@@ -0,0 +1,8 @@
+UPDATE klage.behandling
+SET feilregistrering_registered   = '2023-03-08 13:00:13.000000',
+    feilregistrering_nav_ident    = 'W132204',
+    feilregistrering_reason       = 'Opprettet dobbelt ved en teknisk feil',
+    feilregistrering_fagsystem_id = '23'
+WHERE id in (
+             'dce1b4b8-2ea1-4e47-9e48-627693305460'
+    );
