86fb56a75523d9d5c634ddd428ef8c4acf891531
Andreas Jonsson <andreas.jonsson@nav.no>
2024-05-13 10:30:40 +0200

    More indexes. Show size of egenAnsattMap.
    Co-authored-by: Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>

--- src/main/kotlin/no/nav/klage/oppgave/clients/egenansatt/EgenAnsattService.kt
+++ src/main/kotlin/no/nav/klage/oppgave/clients/egenansatt/EgenAnsattService.kt
@@ -1,5 +1,6 @@
 package no.nav.klage.oppgave.clients.egenansatt
 
+import no.nav.klage.oppgave.util.getLogger
 import org.springframework.stereotype.Service
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentMap
@@ -7,12 +8,18 @@ import java.util.concurrent.ConcurrentMap
 @Service
 class EgenAnsattService {
 
+    companion object {
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        private val logger = getLogger(javaClass.enclosingClass)
+    }
+
     private val egenAnsattMap: ConcurrentMap<String, EgenAnsatt> = ConcurrentHashMap()
 
     fun erEgenAnsatt(foedselsnr: String): Boolean =
         egenAnsattMap[foedselsnr]?.erGyldig() ?: false
 
     fun oppdaterEgenAnsatt(foedselsnr: String, egenAnsatt: EgenAnsatt) {
+        logger.debug("Oppdaterer egenansatt. egenAnsattMap.size=${egenAnsattMap.size}")
         egenAnsattMap[foedselsnr] = egenAnsatt
     }
 }
---
+++ src/main/resources/db/migration/V126__more_indexes.sql
@@ -0,0 +1,2 @@
+CREATE INDEX behandling_tildelt_saksbehandlerident ON klage.behandling (tildelt_saksbehandlerident);
+CREATE INDEX behandling_tildelt_enhet ON klage.behandling (tildelt_enhet);
