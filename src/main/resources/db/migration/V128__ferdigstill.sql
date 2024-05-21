bab20cfbd264eb290c442fddc46fef94f8f184d5
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-05-21 12:33:43 +0200

    Added tildelt_saksbehandlerident.

--- src/main/resources/db/migration/V128__ferdigstill.sql
+++ src/main/resources/db/migration/V128__ferdigstill.sql
@@ -2,5 +2,6 @@ UPDATE klage.behandling
 SET utfall_id = '6',
     dato_behandling_avsluttet = now(),
     dato_behandling_avsluttet_av_saksbehandler = now(),
-    kjennelse_mottatt = now()
+    kjennelse_mottatt = now(),
+    tildelt_saksbehandlerident = 'F128901'
 WHERE id = 'a6304bf6-6c63-4c1c-a2c2-022939df713a';
