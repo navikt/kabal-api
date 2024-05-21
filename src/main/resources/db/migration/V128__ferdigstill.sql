01c324538c241f531f987179be77d4b36de2d2e9
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-05-21 11:52:27 +0200

    Set single behandling to avsluttet, as it was completed outside og Kabal.

---
+++ src/main/resources/db/migration/V128__ferdigstill.sql
@@ -0,0 +1,6 @@
+UPDATE klage.behandling
+SET utfall_id = '6',
+    dato_behandling_avsluttet = now(),
+    dato_behandling_avsluttet_av_saksbehandler = now(),
+    kjennelse_mottatt = now()
+WHERE id = 'a6304bf6-6c63-4c1c-a2c2-022939df713a';
