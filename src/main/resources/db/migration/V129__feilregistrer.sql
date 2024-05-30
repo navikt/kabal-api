921b101213bb79db5a35cffcdd6c32eef6c25d06
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-05-30 15:45:45 +0200

    Remove behandling created by double-clicking.

---
+++ src/main/resources/db/migration/V129__feilregistrer.sql
@@ -0,0 +1,8 @@
+UPDATE klage.behandling
+SET feilregistrering_registered   = '2024-05-30 16:00:13.000000',
+    feilregistrering_nav_ident    = 'W132204',
+    feilregistrering_reason       = 'Opprettet dobbelt ved en teknisk feil',
+    feilregistrering_fagsystem_id = '23'
+WHERE id in (
+             'cffbe325-bc98-44f5-8d77-736abcba5c7b'
+    );
