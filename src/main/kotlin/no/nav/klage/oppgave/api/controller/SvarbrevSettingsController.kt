ef936e8ef8209d7261b926263cb3b7a73a5b2db0
Andreas Jonsson <andreas.jonsson@nav.no>
2024-06-14 20:17:53 +0200

    Add security.

--- src/main/kotlin/no/nav/klage/oppgave/api/controller/SvarbrevSettingsController.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/controller/SvarbrevSettingsController.kt
@@ -1,12 +1,17 @@
 package no.nav.klage.oppgave.api.controller
 
+import io.swagger.v3.oas.annotations.tags.Tag
 import no.nav.klage.oppgave.api.view.SvarbrevSettingsView
 import no.nav.klage.oppgave.api.view.UpdateSvarbrevSettingsInput
+import no.nav.klage.oppgave.config.SecurityConfiguration.Companion.ISSUER_AAD
 import no.nav.klage.oppgave.service.SvarbrevSettingsService
+import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
+@Tag(name = "kabal-api")
+@ProtectedWithClaims(issuer = ISSUER_AAD)
 @RequestMapping("/svarbrev-settings")
 class SvarbrevSettingsController(
     private val svarbrevSettingsService: SvarbrevSettingsService,
