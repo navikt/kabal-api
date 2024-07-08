0ace61a0ee046e77e35ee843c3c01602b958c0aa
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-07-08 11:55:35 +0200

    Handle "Hevet" utfall in AnkeITrygderettenBehandling.
    Co-authored-by: Andreas Jonsson <andreas.jonsson@nav.no>

--- src/main/kotlin/no/nav/klage/oppgave/domain/kafka/ExternalUtfall.kt
+++ src/main/kotlin/no/nav/klage/oppgave/domain/kafka/ExternalUtfall.kt
@@ -12,6 +12,6 @@ enum class ExternalUtfall(val navn: String) {
     INNSTILLING_STADFESTELSE("Innstilling: Stadfestelse"),
     INNSTILLING_AVVIST("Innstilling: Avvist"),
     HENVIST("Henvist"),
-    //TODO: Uavklart hva som skal gjøres for "HEVET" fra TR
+    HEVET("Hevet"),
     ;
 }
