0d10e772ea903419ef8676c6ff74c546f2d2b450
Andreas Jonsson <andreas.jonsson@nav.no>
2024-07-17 09:24:31 +0200

    Debugging "set saksbehandler".

--- src/main/kotlin/no/nav/klage/oppgave/api/controller/BehandlingAssignmentController.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/controller/BehandlingAssignmentController.kt
@@ -44,7 +44,7 @@ class BehandlingAssignmentController(
             logger
         )
 
-        return behandlingService.setSaksbehandler(
+        val saksbehandler = behandlingService.setSaksbehandler(
             behandlingId = behandlingId,
             tildeltSaksbehandlerIdent = saksbehandlerInput.navIdent,
             enhetId = saksbehandlerService.getEnhetForSaksbehandler(
@@ -53,6 +53,10 @@ class BehandlingAssignmentController(
             fradelingReason = null,
             utfoerendeSaksbehandlerIdent = innloggetSaksbehandlerService.getInnloggetIdent(),
         )
+
+        logger.debug("After setting saksbehandler: " + behandlingService.getSaksbehandler(behandlingId))
+
+        return saksbehandler
     }
 
     @PostMapping("/behandlinger/{id}/fradel")
