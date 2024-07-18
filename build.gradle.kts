e61d16db8361ca68751a936def98438cee061e42
Andreas Jonsson <andreas.jonsson@nav.no>
2024-07-18 21:38:25 +0200

    Fixed Flyway upgrade.

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("org.springframework.kafka:spring-kafka")
     implementation("io.projectreactor.kafka:reactor-kafka")
     implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
     implementation("com.zaxxer:HikariCP")
     implementation("org.postgresql:postgresql")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
