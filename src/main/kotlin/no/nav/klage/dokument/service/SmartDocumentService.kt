76a8a76a2f982ed27994e0d6553fa0ea21effe4f
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-09-05 13:17:18 +0200

    TemplateId is required when creating new smartDocument.

--- src/main/kotlin/no/nav/klage/dokument/service/SmartDocumentService.kt
+++ src/main/kotlin/no/nav/klage/dokument/service/SmartDocumentService.kt
@@ -68,7 +68,7 @@ class SmartDocumentService(
         val dokumentType =
             if (input.dokumentTypeId != null) DokumentType.of(input.dokumentTypeId) else DokumentType.VEDTAK
         val language = Language.valueOf(input.language.name)
-        val smartEditorTemplateId = input.templateId ?: error("TODO. Can be null?")
+        val smartEditorTemplateId = input.templateId
 
         val behandling = behandlingService.getBehandlingAndCheckLeseTilgangForPerson(behandlingId)
 
