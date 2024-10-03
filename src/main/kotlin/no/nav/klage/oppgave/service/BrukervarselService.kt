6a614bb89108fff2fa65d23a95d19fd7b5369e08
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-10-03 11:22:41 +0200

    WIP.

--- src/main/kotlin/no/nav/klage/oppgave/service/BrukervarselService.kt
+++ src/main/kotlin/no/nav/klage/oppgave/service/BrukervarselService.kt
@@ -1,11 +1,13 @@
 package no.nav.klage.oppgave.service
 
 import no.nav.klage.dokument.api.view.DokumentView
+import no.nav.klage.kodeverk.Type
 import no.nav.klage.oppgave.domain.events.BehandlingEndretEvent
 import no.nav.klage.oppgave.domain.kafka.EventType
 import no.nav.klage.oppgave.domain.kafka.KafkaEvent
 import no.nav.klage.oppgave.domain.kafka.UtsendingStatus
 import no.nav.klage.oppgave.domain.klage.Behandling
+import no.nav.klage.oppgave.domain.klage.Felt
 import no.nav.klage.oppgave.repositories.KafkaEventRepository
 import no.nav.tms.varsel.action.Sensitivitet
 import no.nav.tms.varsel.action.Tekst
@@ -22,8 +24,9 @@ class BrukervarselService(
 ) {
 
     fun process(behandlingEndretEvent: BehandlingEndretEvent) {
-        if (shouldSendVarsel(behandlingEndretEvent)) {
-            val behandling = behandlingEndretEvent.behandling
+        val payload = getBrukervarselPayload(behandlingEndretEvent)
+
+        if (payload != null) {
             val eventId = UUID.randomUUID()
 
             kafkaEventRepository.save(
@@ -33,15 +36,24 @@ class BrukervarselService(
                     kilde = behandlingEndretEvent.behandling.fagsystem.navn,
                     kildeReferanse = behandlingEndretEvent.behandling.kildeReferanse,
                     status = UtsendingStatus.IKKE_SENDT,
-                    jsonPayload = behandlingOpprettet(behandling),
+                    jsonPayload = payload,
                     type = EventType.BRUKERVARSEL
                 )
             )
         }
     }
 
-    private fun shouldSendVarsel(behandlingEndretEvent: BehandlingEndretEvent): Boolean {
-        return (behandlingEndretEvent.endringslogginnslag.isEmpty())
+    private fun getBrukervarselPayload(behandlingEndretEvent: BehandlingEndretEvent): String? {
+        if (behandlingEndretEvent.behandling.type in listOf(Type.KLAGE, Type.ANKE)) {
+            if (behandlingEndretEvent.endringslogginnslag.isEmpty()) {
+                return behandlingOpprettet(behandling = behandlingEndretEvent.behandling)
+            } else if (behandlingEndretEvent.endringslogginnslag.any {
+                    it.felt === Felt.AVSLUTTET_AV_SAKSBEHANDLER_TIDSPUNKT
+                }) {
+                return behandlingInaktivert(behandling = behandlingEndretEvent.behandling)
+            }
+        }
+        return null
     }
 
     fun behandlingOpprettet(behandling: Behandling): String {
@@ -52,10 +64,39 @@ class BrukervarselService(
             ident = behandling.sakenGjelder.partId.value
             tekster += Tekst(
                 spraakkode = DokumentView.Language.NB.name,
-                tekst = "${behandling.type.name} mottatt hos NAV Klageinstans ${behandling.mottattKlageinstans.toLocalDate()}. Ytelse: ${behandling.ytelse.name}",
+                tekst = "${behandling.type.navn} på ${behandling.ytelse.navn.toSpecialCase()} mottatt hos NAV Klageinstans ${behandling.mottattKlageinstans.toLocalDate()}.",
                 default = true
             )
             aktivFremTil = ZonedDateTime.now(ZoneId.of("Z")).plusDays(1)
+        }
+    }
+
+    fun behandlingInaktivert(behandling: Behandling): String {
+        return VarselActionBuilder.inaktiver {
+            varselId = behandling.id.toString()
+        }
+    }
+
+    private fun String.toSpecialCase(): String {
+        val strings = this.split(" - ")
+        return when (strings.size) {
+            1 -> {
+                replaceFirstChar { it.lowercase(Locale.getDefault()) }
+            }
+
+            2 -> {
+                if (strings[0].equals(other = strings[1], ignoreCase = true)) {
+                    replaceFirstChar { it.lowercase(Locale.getDefault()) }
+                } else {
+                    replaceFirstChar { strings[0].lowercase(Locale.getDefault()) } + " - " + replaceFirstChar {
+                        strings[0].lowercase(
+                            Locale.getDefault()
+                        )
+                    }
+                }
+            }
+
+            else -> this
         }
     }
 }
