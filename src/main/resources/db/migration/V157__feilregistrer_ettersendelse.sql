575c1c1e98255caaf39e045a9333e01b5fdf81d7
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-10-28 07:55:44 +0100

    Correct fagsystem in feilregistrering.

--- src/main/resources/db/migration/V157__feilregistrer_ettersendelse.sql
+++ src/main/resources/db/migration/V157__feilregistrer_ettersendelse.sql
@@ -2,7 +2,7 @@ UPDATE klage.behandling
 SET feilregistrering_registered   = now(),
     feilregistrering_nav_ident    = 'W132204',
     feilregistrering_reason       = 'Skulle egentlig være en ettersendelse',
-    feilregistrering_fagsystem_id = '1'
+    feilregistrering_fagsystem_id = '23'
 WHERE id in (
              '4c104c49-c14e-4648-8da6-d5d122c2e4c0'
     );
