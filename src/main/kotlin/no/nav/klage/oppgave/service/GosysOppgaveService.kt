4e30d9e1a5444f31c0f9fdd21bb5239c4c7f06fe
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-11-13 15:51:54 +0100

    Formatting.
    Co-authored-by: Andreas Jonsson <andreas.jonsson@nav.no>

--- src/main/kotlin/no/nav/klage/oppgave/service/GosysOppgaveService.kt
+++ src/main/kotlin/no/nav/klage/oppgave/service/GosysOppgaveService.kt
@@ -217,7 +217,7 @@ class GosysOppgaveService(
             endretAvEnhetsnr = ENDRET_AV_ENHETSNR_SYSTEM,
             status = Status.FERDIGSTILT,
             kommentar = Kommentar(
-                tekst = "Klageinstansen har ferdigstilt behandlingen i Kabal med utfall ${behandling.utfall!!.navn}.",
+                tekst = "Klageinstansen har ferdigstilt behandlingen i Kabal med utfall: ${behandling.utfall!!.navn}.",
                 automatiskGenerert = true
             )
         )
