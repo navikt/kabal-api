8168dade0b0d6fa3173df77c314ea2fe6e9259fb
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-11-13 15:00:38 +0100

    New token support and spring boot, filter for sse.
    Co-authored-by: Andreas Jonsson <andreas.jonsson@nav.no>

--- src/main/kotlin/no/nav/klage/oppgave/util/TokenUtil.kt
+++ src/main/kotlin/no/nav/klage/oppgave/util/TokenUtil.kt
@@ -22,175 +22,175 @@ class TokenUtil(
     fun getSaksbehandlerAccessTokenWithGraphScope(): String {
         val clientProperties = clientConfigurationProperties.registration["azure-onbehalfof"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getSaksbehandlerAccessTokenWithPdlScope(): String {
         val clientProperties = clientConfigurationProperties.registration["pdl-onbehalfof"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getSaksbehandlerAccessTokenWithKodeverkScope(): String {
         val clientProperties = clientConfigurationProperties.registration["kodeverk-onbehalfof"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getAppAccessTokenWithPdlScope(): String {
         val clientProperties = clientConfigurationProperties.registration["pdl-maskintilmaskin"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getSaksbehandlerAccessTokenWithSafScope(): String {
         val clientProperties = clientConfigurationProperties.registration["saf-onbehalfof"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getAppAccessTokenWithSafScope(): String {
         val clientProperties = clientConfigurationProperties.registration["saf-maskintilmaskin"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getSaksbehandlerAccessTokenWithDokarkivScope(): String {
         val clientProperties = clientConfigurationProperties.registration["dokarkiv-onbehalfof"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getSaksbehandlerAccessTokenWithKabalFileApiScope(): String {
         val clientProperties = clientConfigurationProperties.registration["kabal-file-api-onbehalfof"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getAppAccessTokenWithKabalFileApiScope(): String {
         val clientProperties = clientConfigurationProperties.registration["kabal-file-api-maskintilmaskin"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getSaksbehandlerAccessTokenWithKabalDocumentScope(): String {
         val clientProperties = clientConfigurationProperties.registration["kabal-document-onbehalfof"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getSaksbehandlerAccessTokenWithKabalSmartEditorApiScope(): String {
         val clientProperties = clientConfigurationProperties.registration["kabal-smart-editor-api-onbehalfof"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getAppAccessTokenWithKabalSmartEditorApiScope(): String {
         val clientProperties = clientConfigurationProperties.registration["kabal-smart-editor-api-maskintilmaskin"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getAppAccessTokenWithKabalDocumentScope(): String {
         val clientProperties = clientConfigurationProperties.registration["kabal-document-maskintilmaskin"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getSaksbehandlerAccessTokenWithKakaApiScope(): String {
         val clientProperties = clientConfigurationProperties.registration["kaka-api-onbehalfof"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getAppAccessTokenWithKakaApiScope(): String {
         val clientProperties = clientConfigurationProperties.registration["kaka-api-maskintilmaskin"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getAppAccessTokenWithGraphScope(): String {
         val clientProperties = clientConfigurationProperties.registration["app"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getUserAccessTokenWithKabalInnstillingerScope(): String {
         val clientProperties = clientConfigurationProperties.registration["kabal-innstillinger-onbehalfof"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getSkjermedeAccessToken(): String {
         val clientProperties = clientConfigurationProperties.registration["skjermede-maskintilmaskin"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getOnBehalfOfTokenWithRegoppslagScope(): String {
         val clientProperties = clientConfigurationProperties.registration["regoppslag-onbehalfof"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getAppAccessTokenWithRegoppslagScope(): String {
         val clientProperties = clientConfigurationProperties.registration["regoppslag-maskintilmaskin"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getOnBehalfOfTokenWithDokDistKanalScope(): String {
         val clientProperties = clientConfigurationProperties.registration["dok-dist-kanal-onbehalfof"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getAppAccessTokenWithDokDistKanalScope(): String {
         val clientProperties = clientConfigurationProperties.registration["dok-dist-kanal-maskintilmaskin"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getOnBehalfOfTokenWithKrrProxyScope(): String {
         val clientProperties = clientConfigurationProperties.registration["krr-proxy-onbehalfof"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getAppAccessTokenWithKrrProxyScope(): String {
         val clientProperties = clientConfigurationProperties.registration["krr-proxy-maskintilmaskin"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getOnBehalfOfTokenWithKlageFSSProxyScope(): String {
         val clientProperties = clientConfigurationProperties.registration["klage-fss-proxy-onbehalfof"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getAppAccessTokenWithKlageFSSProxyScope(): String {
         val clientProperties = clientConfigurationProperties.registration["klage-fss-proxy-maskintilmaskin"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getAppAccessTokenWithNomScope(): String {
         val clientProperties = clientConfigurationProperties.registration["nom-maskintilmaskin"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getSaksbehandlerAccessTokenWithGosysOppgaveScope(): String {
         val clientProperties = clientConfigurationProperties.registration["gosys-oppgave-onbehalfof"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getAppAccessTokenWithGosysOppgaveScope(): String {
         val clientProperties = clientConfigurationProperties.registration["gosys-oppgave-maskintilmaskin"]!!
         val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        return response.accessToken!!
+        return response.access_token!!
     }
 
     fun getAccessTokenFrontendSent(): String =
