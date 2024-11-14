9efe5ed8e3fe1aa75ae5d8da8b5b54761b0a7563
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-11-14 11:27:10 +0100

    Test Jetty instead of Tomcat.
    Co-authored-by: Andreas Jonsson <andreas.jonsson@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,12 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-jetty")
+    modules {
+        module("org.springframework.boot:spring-boot-starter-tomcat") {
+            replacedBy("org.springframework.boot:spring-boot-starter-jetty")
+        }
+    }
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-validation")
