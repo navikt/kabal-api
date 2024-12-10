3b5c2cb5890d39035af982e6431166b0d42be502
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-12-10 21:39:26 +0100

    test

--- src/main/kotlin/no/nav/klage/innsyn/api/controller/InnsynController.kt
+++ src/main/kotlin/no/nav/klage/innsyn/api/controller/InnsynController.kt
@@ -19,8 +19,8 @@ import org.springframework.web.bind.annotation.RestController
     name = "kabal-innsyn",
     description = "api for innsyn i brukeres saker"
 )
-@ProtectedWithClaims(issuer = SecurityConfiguration.ISSUER_AAD)
-//@ProtectedWithClaims(issuer = SecurityConfiguration.TOKEN_X, claimMap = ["acr=Level4", "acr=idporten-loa-high"])
+//@ProtectedWithClaims(issuer = SecurityConfiguration.ISSUER_AAD)
+@ProtectedWithClaims(issuer = SecurityConfiguration.TOKEN_X, claimMap = ["acr=Level4", "acr=idporten-loa-high"])
 @RequestMapping("api/innsyn")
 class InnsynController(
     private val innsynService: InnsynService,
