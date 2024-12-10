04652affc1cada655ca55ab5c6275d8ba68169c2
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-12-10 21:47:05 +0100

    test

--- src/main/kotlin/no/nav/klage/innsyn/api/controller/InnsynController.kt
+++ src/main/kotlin/no/nav/klage/innsyn/api/controller/InnsynController.kt
@@ -9,7 +9,6 @@ import no.nav.klage.oppgave.util.TokenUtil
 import no.nav.klage.oppgave.util.getLogger
 import no.nav.klage.oppgave.util.getSecureLogger
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -34,7 +33,7 @@ class InnsynController(
     }
 
     //Unprotected while developing
-    @Unprotected
+//    @Unprotected
     @Operation(
         summary = "Hent en brukers saker",
         description = "Hent en brukers saker, basert på brukerens ident hentet fra token"
