19772c1033a979dcf4a59799e7f69c198c96da08
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-01-16 13:47:18 +0100

    Unused class.

--- src/main/kotlin/no/nav/klage/dokument/domain/dokumenterunderarbeid/JournalfoertDokumentReference.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.klage.dokument.domain.dokumenterunderarbeid
-
-import jakarta.persistence.Embeddable
-
-@Embeddable
-data class JournalfoertDokumentReference(
-    val journalpostId: String,
-    val dokumentInfoId: String,
-)
