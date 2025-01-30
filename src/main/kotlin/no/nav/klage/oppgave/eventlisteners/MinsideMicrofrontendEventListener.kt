05faaec41ed9f74d966f421f6b396c7a5d52524b
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-01-30 14:29:21 +0100

    Disable eventlistener for now.

--- src/main/kotlin/no/nav/klage/oppgave/eventlisteners/MinsideMicrofrontendEventListener.kt
+++ src/main/kotlin/no/nav/klage/oppgave/eventlisteners/MinsideMicrofrontendEventListener.kt
@@ -3,7 +3,6 @@ package no.nav.klage.oppgave.eventlisteners
 import no.nav.klage.oppgave.domain.events.BehandlingEndretEvent
 import no.nav.klage.oppgave.service.MinsideMicrofrontendService
 import no.nav.klage.oppgave.util.getLogger
-import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Service
 
 @Service
@@ -16,7 +15,8 @@ class MinsideMicrofrontendEventListener(
         private val logger = getLogger(javaClass.enclosingClass)
     }
 
-    @EventListener
+    //TODO: Uncomment when minside microfrontend is ready
+//    @EventListener
     fun handleMinsideMicrofrontendEvent(behandlingEndretEvent: BehandlingEndretEvent) {
         logger.debug(
             "Received BehandlingEndretEvent for behandlingId {} in {}",
