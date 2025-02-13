e1da2ea164af826a931b87b8759a509bc63e80f3
Andreas Jonsson <andreas.jonsson@nav.no>
2025-02-13 14:04:05 +0100

    Using aud-tables more or less like Hibernate would create them.

--- src/main/resources/db/migration/V188__add_audit_tables.sql
+++ src/main/resources/db/migration/V188__add_audit_tables.sql
@@ -1,9 +1,9 @@
 create table klage.revision
 (
     id        bigint not null,
-    timestamp bigint  not null,
     actor     text,
     request   text,
+    timestamp bigint,
     primary key (id)
 );
 
@@ -11,121 +11,99 @@ create sequence klage.revision_seq start with 1 increment by 1;
 
 create table klage.behandling_aud
 (
-    id                                         uuid      not null,
+    id                                         uuid   not null,
+    rev                                        bigint not null,
+    behandling_type                            text   not null,
+    revtype                                    smallint,
+    created                                    timestamp,
+    dvh_referanse                              text,
+    sak_fagsak_id                              text,
+    sak_fagsystem                              text,
+    feilregistrering_fagsystem_id              text,
+    feilregistrering_nav_ident                 text,
+    feilregistrering_navn                      text,
+    feilregistrering_reason                    text,
+    feilregistrering_registered                timestamp,
+    dato_behandling_avsluttet                  timestamp,
+    dato_behandling_avsluttet_av_saksbehandler timestamp,
+    ferdigstilling_nav_ident                   text,
+    ferdigstilling_navn                        text,
+    frist                                      date,
+    gosys_oppgave_id                           bigint,
+    oppgave_returned_kommentar                 text,
+    oppgave_returned_mappe_id                  bigint,
+    oppgave_returned_tildelt_enhetsnummer      text,
+    ignore_gosys_oppgave                       boolean,
+    kilde_referanse                            text,
     klager_type                                text,
     klager_value                               text,
-    prosessfullmektig_type                     text,
-    prosessfullmektig_value                    text,
-    saken_gjelder_type                         text,
-    saken_gjelder_value                        text,
-    tema_id                                    text,
-    type_id                                    text      not null,
-    kilde_referanse                            text      not null,
-    sak_fagsystem                              text      not null,
-    sak_fagsak_id                              text,
-    dato_mottatt_foersteinstans                date,
-    dato_mottatt_klageinstans                  timestamp not null,
-    dato_behandling_tildelt                    timestamp,
-    frist                                      date,
-    tildelt_saksbehandlerident                 text,
-    tildelt_enhet                              text,
-    avsender_enhet_foersteinstans              text,
-    previous_saksbehandlerident                text,
-    mottak_id                                  uuid,
-    kommentar_fra_foersteinstans               text,
-    created                                    timestamp not null,
-    modified                                   timestamp not null,
-    medunderskriver_enhet                      text,
-    kaka_kvalitetsvurdering_id                 uuid,
-    ytelse_id                                  text,
-    dvh_referanse                              text,
-    behandling_type                            text      not null,
-    klage_vedtaks_dato                         date,
-    klage_behandlende_enhet                    text,
-    source_behandling_id                       uuid,
-    satt_paa_vent_from                         date,
-    sendt_til_trygderetten                     timestamp,
-    kjennelse_mottatt                          timestamp,
-    kaka_kvalitetsvurdering_version            integer   not null,
-    feilregistrering_nav_ident                 text,
-    feilregistrering_registered                timestamp,
-    feilregistrering_reason                    text,
-    feilregistrering_fagsystem_id              text,
-    satt_paa_vent_to                           date,
-    satt_paa_vent_reason                       text,
-    utfall_id                                  text,
-    dato_behandling_avsluttet_av_saksbehandler timestamp,
-    dato_behandling_avsluttet                  timestamp,
     medunderskriverident                       text,
     dato_sendt_medunderskriver                 timestamp,
     medunderskriver_flow_state_id              text,
-    rol_ident                                  text,
-    rol_flow_state_id                          text      not null,
-    ny_ankebehandling_ka                       timestamp,
-    rol_returned_date                          timestamp,
-    gosys_oppgave_id                           bigint,
-    varslet_frist                              date,
-    varslet_behandlingstid_units               integer,
-    varslet_behandlingstid_unit_type_id        text,
-    feilregistrering_navn                      text,
-    ferdigstilling_nav_ident                   text,
-    ferdigstilling_navn                        text,
-    oppgave_returned_tildelt_enhetsnummer      text,
-    oppgave_returned_mappe_id                  bigint,
-    oppgave_returned_kommentar                 text,
-    ny_behandling_etter_tr_opphevet            timestamp,
-    anke_behandlende_enhet                     text,
-    tilbakekreving                             boolean,
-    ignore_gosys_oppgave                       boolean,
-    prosessfullmektig_navn                     text,
-    prosessfullmektig_address_adressetype      text,
+    modified                                   timestamp,
+    dato_mottatt_klageinstans                  timestamp,
+    previous_saksbehandlerident                text,
     prosessfullmektig_address_adresselinje_1   text,
     prosessfullmektig_address_adresselinje_2   text,
     prosessfullmektig_address_adresselinje_3   text,
+    prosessfullmektig_address_landkode         text,
     prosessfullmektig_address_postnummer       text,
     prosessfullmektig_address_poststed         text,
-    prosessfullmektig_address_landkode         text,
-    rev                                        bigint   not null,
-    revtype                                    integer,
+    prosessfullmektig_navn                     text,
+    prosessfullmektig_type                     text,
+    prosessfullmektig_value                    text,
+    rol_flow_state_id                          text,
+    rol_ident                                  text,
+    rol_returned_date                          timestamp,
+    saken_gjelder_type                         text,
+    saken_gjelder_value                        text,
+    satt_paa_vent_from                         date,
+    satt_paa_vent_reason                       text,
+    satt_paa_vent_to                           date,
+    tilbakekreving                             boolean,
+    tildelt_enhet                              text,
+    tildelt_saksbehandlerident                 text,
+    dato_behandling_tildelt                    timestamp,
+    type_id                                    text,
+    utfall_id                                  text,
+    ytelse_id                                  text,
     primary key (rev, id)
 );
 
 create table klage.behandling_extra_utfall_aud
 (
-    id            text    not null,
-    behandling_id uuid    not null,
     rev           bigint not null,
-    revtype       integer,
-    primary key (rev, id, behandling_id)
+    behandling_id uuid   not null,
+    id            text   not null,
+    revtype       smallint,
+    primary key (behandling_id, rev, id)
+);
+
+create table klage.behandling_extra_utfall_aud
+(
+    rev           bigint not null,
+    behandling_id uuid   not null,
+    id            text   not null,
+    revtype       smallint,
+    primary key (behandling_id, rev, id)
 );
 
 create table klage.behandling_hjemmel_aud
 (
-    id            text    not null,
-    behandling_id uuid    not null,
     rev           bigint not null,
-    revtype       integer,
-    primary key (rev, id, behandling_id)
+    behandling_id uuid   not null,
+    id            text   not null,
+    revtype       smallint,
+    primary key (behandling_id, rev, id)
 );
 
 create table klage.behandling_registreringshjemmel_aud
 (
-    id            text    not null,
-    behandling_id uuid    not null,
     rev           bigint not null,
-    revtype       integer,
-    primary key (rev, id, behandling_id)
-);
-
-create table klage.saksdokument_aud
-(
-    id               uuid    not null,
-    behandling_id    uuid    not null,
-    journalpost_id   text,
-    dokument_info_id text,
-    rev              bigint not null,
-    revtype          integer,
-    primary key (rev, id, behandling_id)
+    behandling_id uuid   not null,
+    id            text   not null,
+    revtype       smallint,
+    primary key (behandling_id, rev, id)
 );
 
 create table klage.behandling_saksdokument_aud
@@ -135,6 +113,16 @@ create table klage.behandling_saksdokument_aud
     id            uuid   not null,
     revtype       smallint,
     primary key (behandling_id, rev, id)
+);
+
+create table klage.saksdokument_aud
+(
+    id               uuid   not null,
+    rev              bigint not null,
+    revtype          smallint,
+    dokument_info_id text,
+    journalpost_id   text,
+    primary key (rev, id)
 );
 
 alter table klage.behandling_aud
