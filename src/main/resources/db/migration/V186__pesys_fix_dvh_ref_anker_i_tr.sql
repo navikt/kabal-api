5626036ccc42bd753f58cbc434fabab18f026aa5
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-02-13 12:34:17 +0100

    Patch wrong values in anker i TR migrated from Pesys.

---
+++ src/main/resources/db/migration/V186__pesys_fix_dvh_ref_anker_i_tr.sql
@@ -0,0 +1,216 @@
+UPDATE klage.behandling
+SET dvh_referanse = '46924798'
+WHERE kilde_referanse = '42732182'
+  AND dvh_referanse = '29929939';
+UPDATE klage.behandling
+SET dvh_referanse = '37553958'
+WHERE kilde_referanse = '50839922'
+  AND dvh_referanse = '34591160';
+UPDATE klage.behandling
+SET dvh_referanse = '37042716'
+WHERE kilde_referanse = '51000174'
+  AND dvh_referanse = '35971378';
+UPDATE klage.behandling
+SET dvh_referanse = '44906368'
+WHERE kilde_referanse = '51237578'
+  AND dvh_referanse = '36204480';
+UPDATE klage.behandling
+SET dvh_referanse = '39240108'
+WHERE kilde_referanse = '54868291'
+  AND dvh_referanse = '36297746';
+UPDATE klage.behandling
+SET dvh_referanse = '46731299'
+WHERE kilde_referanse = '56015005'
+  AND dvh_referanse = '36916837';
+UPDATE klage.behandling
+SET dvh_referanse = '40490013'
+WHERE kilde_referanse = '58105978'
+  AND dvh_referanse = '39430889';
+UPDATE klage.behandling
+SET dvh_referanse = '41056652'
+WHERE kilde_referanse = '58123865'
+  AND dvh_referanse = '39569750';
+UPDATE klage.behandling
+SET dvh_referanse = '42409863'
+WHERE kilde_referanse = '58164030'
+  AND dvh_referanse = '39290541';
+UPDATE klage.behandling
+SET dvh_referanse = '48464095'
+WHERE kilde_referanse = '58242017'
+  AND dvh_referanse = '39419643';
+UPDATE klage.behandling
+SET dvh_referanse = '43360766'
+WHERE kilde_referanse = '59562571'
+  AND dvh_referanse = '40296601';
+UPDATE klage.behandling
+SET dvh_referanse = '44368650'
+WHERE kilde_referanse = '59570942'
+  AND dvh_referanse = '40394607';
+UPDATE klage.behandling
+SET dvh_referanse = '46795526'
+WHERE kilde_referanse = '59677898'
+  AND dvh_referanse = '42524508';
+UPDATE klage.behandling
+SET dvh_referanse = '48372473'
+WHERE kilde_referanse = '59858460'
+  AND dvh_referanse = '42763873';
+UPDATE klage.behandling
+SET dvh_referanse = '48386800'
+WHERE kilde_referanse = '61798791'
+  AND dvh_referanse = '43016284';
+UPDATE klage.behandling
+SET dvh_referanse = '47032750'
+WHERE kilde_referanse = '62711221'
+  AND dvh_referanse = '45180705';
+UPDATE klage.behandling
+SET dvh_referanse = '50200505'
+WHERE kilde_referanse = '62762092'
+  AND dvh_referanse = '44771630';
+UPDATE klage.behandling
+SET dvh_referanse = '49229515'
+WHERE kilde_referanse = '62770112'
+  AND dvh_referanse = '44874762';
+UPDATE klage.behandling
+SET dvh_referanse = '48803016'
+WHERE kilde_referanse = '62771621'
+  AND dvh_referanse = '45698696';
+UPDATE klage.behandling
+SET dvh_referanse = '46705285'
+WHERE kilde_referanse = '63239959'
+  AND dvh_referanse = '44511107';
+UPDATE klage.behandling
+SET dvh_referanse = '48211098'
+WHERE kilde_referanse = '63252563'
+  AND dvh_referanse = '45155349';
+UPDATE klage.behandling
+SET dvh_referanse = '48273054'
+WHERE kilde_referanse = '65002130'
+  AND dvh_referanse = '46685908';
+UPDATE klage.behandling
+SET dvh_referanse = '49153114'
+WHERE kilde_referanse = '65025720'
+  AND dvh_referanse = '46592319';
+UPDATE klage.behandling
+SET dvh_referanse = '47165156'
+WHERE kilde_referanse = '65043518'
+  AND dvh_referanse = '46863905';
+UPDATE klage.behandling
+SET dvh_referanse = '47789603'
+WHERE kilde_referanse = '65079148'
+  AND dvh_referanse = '46761693';
+UPDATE klage.behandling
+SET dvh_referanse = '48279856'
+WHERE kilde_referanse = '65092341'
+  AND dvh_referanse = '46581200';
+UPDATE klage.behandling
+SET dvh_referanse = '48632600'
+WHERE kilde_referanse = '65110126'
+  AND dvh_referanse = '46796956';
+UPDATE klage.behandling
+SET dvh_referanse = '48360549'
+WHERE kilde_referanse = '65118462'
+  AND dvh_referanse = '46966952';
+UPDATE klage.behandling
+SET dvh_referanse = '49775165'
+WHERE kilde_referanse = '65229724'
+  AND dvh_referanse = '46871851';
+UPDATE klage.behandling
+SET dvh_referanse = '48655923'
+WHERE kilde_referanse = '65515231'
+  AND dvh_referanse = '46987163';
+UPDATE klage.behandling
+SET dvh_referanse = '49189245'
+WHERE kilde_referanse = '65515785'
+  AND dvh_referanse = '46831512';
+UPDATE klage.behandling
+SET dvh_referanse = '48610584'
+WHERE kilde_referanse = '65519983'
+  AND dvh_referanse = '47050347';
+UPDATE klage.behandling
+SET dvh_referanse = '48656851'
+WHERE kilde_referanse = '65665971'
+  AND dvh_referanse = '47112614';
+UPDATE klage.behandling
+SET dvh_referanse = '48830671'
+WHERE kilde_referanse = '65677516'
+  AND dvh_referanse = '47221565';
+UPDATE klage.behandling
+SET dvh_referanse = '49064507'
+WHERE kilde_referanse = '65677635'
+  AND dvh_referanse = '47223409';
+UPDATE klage.behandling
+SET dvh_referanse = '48815445'
+WHERE kilde_referanse = '65677689'
+  AND dvh_referanse = '47232933';
+UPDATE klage.behandling
+SET dvh_referanse = '49083804'
+WHERE kilde_referanse = '65694656'
+  AND dvh_referanse = '47195122';
+UPDATE klage.behandling
+SET dvh_referanse = '49088281'
+WHERE kilde_referanse = '65697538'
+  AND dvh_referanse = '47676204';
+UPDATE klage.behandling
+SET dvh_referanse = '48496134'
+WHERE kilde_referanse = '65881836'
+  AND dvh_referanse = '47075843';
+UPDATE klage.behandling
+SET dvh_referanse = '49120882'
+WHERE kilde_referanse = '66015487'
+  AND dvh_referanse = '47789246';
+UPDATE klage.behandling
+SET dvh_referanse = '48656611'
+WHERE kilde_referanse = '66037964'
+  AND dvh_referanse = '47677558';
+UPDATE klage.behandling
+SET dvh_referanse = '49081011'
+WHERE kilde_referanse = '66056048'
+  AND dvh_referanse = '48191344';
+UPDATE klage.behandling
+SET dvh_referanse = '48376197'
+WHERE kilde_referanse = '66074680'
+  AND dvh_referanse = '48226548';
+UPDATE klage.behandling
+SET dvh_referanse = '49420080'
+WHERE kilde_referanse = '66079224'
+  AND dvh_referanse = '48235012';
+UPDATE klage.behandling
+SET dvh_referanse = '49128417'
+WHERE kilde_referanse = '66080169'
+  AND dvh_referanse = '48230503';
+UPDATE klage.behandling
+SET dvh_referanse = '49091125'
+WHERE kilde_referanse = '66089585'
+  AND dvh_referanse = '48253006';
+UPDATE klage.behandling
+SET dvh_referanse = '48816117'
+WHERE kilde_referanse = '66089958'
+  AND dvh_referanse = '48020994';
+UPDATE klage.behandling
+SET dvh_referanse = '49088709'
+WHERE kilde_referanse = '66097917'
+  AND dvh_referanse = '46852801';
+UPDATE klage.behandling
+SET dvh_referanse = '49120897'
+WHERE kilde_referanse = '66105405'
+  AND dvh_referanse = '47794890';
+UPDATE klage.behandling
+SET dvh_referanse = '49182562'
+WHERE kilde_referanse = '66141305'
+  AND dvh_referanse = '48348749';
+UPDATE klage.behandling
+SET dvh_referanse = '49219505'
+WHERE kilde_referanse = '66218596'
+  AND dvh_referanse = '48492846';
+UPDATE klage.behandling
+SET dvh_referanse = '49054828'
+WHERE kilde_referanse = '66235147'
+  AND dvh_referanse = '48412294';
+UPDATE klage.behandling
+SET dvh_referanse = '49189586'
+WHERE kilde_referanse = '68380341'
+  AND dvh_referanse = '48396802';
+UPDATE klage.behandling
+SET dvh_referanse = '49420081'
+WHERE kilde_referanse = '68400811'
+  AND dvh_referanse = '49190951';
