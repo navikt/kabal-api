b310d79345df68ff36f0c16a3e2d7cd21e4f3195
Andreas Jonsson <andreas.jonsson@nav.no>
2025-02-13 09:58:06 +0100

    Trying to override how to get the sequence.

--- src/main/kotlin/no/nav/klage/oppgave/domain/OurRevision.kt
+++ src/main/kotlin/no/nav/klage/oppgave/domain/OurRevision.kt
@@ -1,15 +1,22 @@
 package no.nav.klage.oppgave.domain
 
-import jakarta.persistence.Entity
-import jakarta.persistence.Table
+import jakarta.persistence.*
 import no.nav.klage.oppgave.eventlisteners.OurRevisionListener
 import org.hibernate.envers.DefaultRevisionEntity
 import org.hibernate.envers.RevisionEntity
+import org.hibernate.envers.RevisionNumber
+import org.hibernate.envers.RevisionTimestamp
 
 @Entity
 @Table(name = "revision", schema = "klage")
 @RevisionEntity(OurRevisionListener::class)
 class OurRevision(
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "revision_seq")
+    @RevisionNumber
+    var id: Long?,
+    @RevisionTimestamp
+    var timestamp: Long?,
     var actor: String,
     var request: String?,
 ) : DefaultRevisionEntity()
