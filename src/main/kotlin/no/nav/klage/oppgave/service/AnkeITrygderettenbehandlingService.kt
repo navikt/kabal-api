0d7d7808fcf9d34db31215687d7b83b112a84856
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-02-14 07:55:41 +0100

    Always send dvh message SENDT_TIL_TR when receiving anke i TR from external API.

--- src/main/kotlin/no/nav/klage/oppgave/service/AnkeITrygderettenbehandlingService.kt
+++ src/main/kotlin/no/nav/klage/oppgave/service/AnkeITrygderettenbehandlingService.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import no.nav.klage.dokument.api.view.JournalfoertDokumentReference
+import no.nav.klage.kodeverk.Type
 import no.nav.klage.kodeverk.hjemmel.Hjemmel
 import no.nav.klage.kodeverk.hjemmel.ytelseToRegistreringshjemlerV2
 import no.nav.klage.oppgave.api.view.OversendtAnkeITrygderettenV1
@@ -17,10 +18,12 @@ import no.nav.klage.oppgave.domain.klage.Felt
 import no.nav.klage.oppgave.repositories.AnkeITrygderettenbehandlingRepository
 import no.nav.klage.oppgave.repositories.KafkaEventRepository
 import no.nav.klage.oppgave.util.getLogger
+import no.nav.klage.oppgave.util.ourJacksonObjectMapper
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
 import java.util.*
 
 @Service
@@ -37,6 +40,7 @@ class AnkeITrygderettenbehandlingService(
     companion object {
         @Suppress("JAVA_CLASS_ON_COMPANION")
         private val logger = getLogger(javaClass.enclosingClass)
+        private val objectMapper = ourJacksonObjectMapper()
         private val objectMapperBehandlingEvents = ObjectMapper().registerModule(JavaTimeModule()).configure(
             SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false
         )
@@ -143,12 +147,58 @@ class AnkeITrygderettenbehandlingService(
         return ankeITrygderettenbehandling
     }
 
-    fun createAnkeITrygderettenbehandling(input: OversendtAnkeITrygderettenV1) {
+    fun createAnkeITrygderettenbehandlingFromExternalApi(input: OversendtAnkeITrygderettenV1) {
         mottakService.validateAnkeITrygderettenV1(input)
         val inputDocuments =
             dokumentService.createSaksdokumenterFromJournalpostIdList(input.tilknyttedeJournalposter.map { it.journalpostId })
-        createAnkeITrygderettenbehandling(
+        val ankeITrygderettenbehandling = createAnkeITrygderettenbehandling(
             input.createAnkeITrygderettenbehandlingInput(inputDocuments)
         )
+
+        val statistikkTilDVH = StatistikkTilDVH(
+            eventId = UUID.randomUUID(),
+            behandlingId = ankeITrygderettenbehandling.dvhReferanse,
+            behandlingIdKabal = ankeITrygderettenbehandling.toString(),
+            //Means enhetTildeltDato
+            behandlingStartetKA = null,
+            ansvarligEnhetKode = "TR0000",
+            behandlingStatus = BehandlingState.SENDT_TIL_TR,
+            behandlingType = Type.ANKE.name,
+            //Means medunderskriver
+            beslutter = null,
+            endringstid = ankeITrygderettenbehandling.sendtTilTrygderetten,
+            hjemmel = emptyList(),
+            klager = getDVHPart(
+                type = ankeITrygderettenbehandling.klager.partId.type,
+                value = ankeITrygderettenbehandling.klager.partId.value
+            ),
+            opprinneligFagsaksystem = ankeITrygderettenbehandling.fagsystem.navn,
+            overfoertKA = ankeITrygderettenbehandling.mottattKlageinstans.toLocalDate(),
+            resultat = null,
+            sakenGjelder = getDVHPart(
+                type = ankeITrygderettenbehandling.sakenGjelder.partId.type,
+                value = ankeITrygderettenbehandling.sakenGjelder.partId.value
+            ),
+            saksbehandler = ankeITrygderettenbehandling.tildeling?.saksbehandlerident,
+            saksbehandlerEnhet =  ankeITrygderettenbehandling.tildeling?.enhet,
+            tekniskTid = LocalDateTime.now(),
+            vedtaksdato = null,
+            ytelseType = ankeITrygderettenbehandling.ytelse.name,
+            opprinneligFagsakId = ankeITrygderettenbehandling.fagsakId,
+        )
+
+        kafkaEventRepository.save(
+            KafkaEvent(
+                id = UUID.randomUUID(),
+                behandlingId = ankeITrygderettenbehandling.id,
+                kilde = ankeITrygderettenbehandling.fagsystem.navn,
+                kildeReferanse = ankeITrygderettenbehandling.kildeReferanse,
+                status = UtsendingStatus.IKKE_SENDT,
+                jsonPayload = statistikkTilDVH.toJson(),
+                type = EventType.STATS_DVH
+            )
+        )
     }
+
+    private fun StatistikkTilDVH.toJson(): String = objectMapper.writeValueAsString(this)
 }
