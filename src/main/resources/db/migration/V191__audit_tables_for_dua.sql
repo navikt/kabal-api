a68c477a42ff3e4bbdeb20fc1bdeedff4215b110
Andreas Jonsson <andreas.jonsson@nav.no>
2025-02-17 18:29:26 +0100

    Removed unnecessary checks.

--- src/main/resources/db/migration/V191__audit_tables_for_dua.sql
+++ src/main/resources/db/migration/V191__audit_tables_for_dua.sql
@@ -7,15 +7,14 @@ create table klage.dokument_under_arbeid_aud
     behandling_id                          uuid,
     created                                timestamp,
     creator_ident                          text,
-    creator_role                           text check (creator_role in ('KABAL_SAKSBEHANDLING', 'KABAL_ROL',
-                                                                        'KABAL_MEDUNDERSKRIVER', 'NONE')),
+    creator_role                           text,
     ferdigstilt                            timestamp,
     markert_ferdig                         timestamp,
     markert_ferdig_by                      text,
     modified                               timestamp,
     name                                   text,
     parent_id                              uuid,
-    language                               text check (language in ('NN', 'NB')),
+    language                               text,
     mellomlager_id                         text,
     mellomlagret_date                      timestamp,
     mellomlagret_version                   integer,
@@ -30,7 +29,7 @@ create table klage.dokument_under_arbeid_aud
     dokument_type_id                       text,
     journalfoerende_enhet_id               text,
     dato_mottatt                           date,
-    inngaaende_kanal                       text check (inngaaende_kanal in ('ALTINN_INNBOKS', 'E_POST')),
+    inngaaende_kanal                       text,
     primary key (rev, id)
 );
 
