8ee88180fccb74835277df1834ff1a433e4e1d1d
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-03-07 10:07:53 +0100

    Different sorting approach for avslutning.

--- src/main/kotlin/no/nav/klage/oppgave/service/BehandlingService.kt
+++ src/main/kotlin/no/nav/klage/oppgave/service/BehandlingService.kt
@@ -1805,6 +1805,7 @@ class BehandlingService(
     @Transactional(readOnly = true)
     fun findBehandlingerForAvslutning(): List<Pair<UUID, Type>> =
         behandlingRepository.findByFerdigstillingAvsluttetIsNullAndFerdigstillingAvsluttetAvSaksbehandlerIsNotNullAndFeilregistreringIsNull()
+            .sortedByDescending { it.ferdigstilling?.avsluttetAvSaksbehandler }
             .map { it.id to it.type }
 
     fun getPotentialSaksbehandlereForBehandling(behandlingId: UUID): Saksbehandlere {
