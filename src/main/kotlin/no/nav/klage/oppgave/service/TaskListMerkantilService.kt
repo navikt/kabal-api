e1653252fbe23dc1cac657130a6a0182f56db6b3
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-04-10 07:31:11 +0200

    Missing service file in previous commit.

---
+++ src/main/kotlin/no/nav/klage/oppgave/service/TaskListMerkantilService.kt
@@ -0,0 +1,47 @@
+package no.nav.klage.oppgave.service
+
+import no.nav.klage.oppgave.domain.klage.TaskListMerkantil
+import no.nav.klage.oppgave.repositories.TaskListMerkantilRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
+import java.util.*
+
+@Service
+class TaskListMerkantilService(
+    private val taskListMerkantilRepository: TaskListMerkantilRepository,
+    private val innloggetSaksbehandlerService: InnloggetSaksbehandlerService,
+    private val saksbehandlerService: SaksbehandlerService,
+) {
+
+    @Transactional
+    fun createTaskForMerkantil(behandlingId: UUID, reason: String) {
+        taskListMerkantilRepository.save(
+            TaskListMerkantil(
+                behandlingId = behandlingId,
+                reason = reason,
+                created = LocalDateTime.now(),
+                dateHandled = null,
+                handledBy = null,
+                handledByName = null,
+                comment = null,
+            )
+        )
+    }
+
+    @Transactional
+    fun setCommentAndMarkTaskAsCompleted(taskId: UUID, inputComment: String) {
+        val task = taskListMerkantilRepository.findById(taskId)
+            .orElseThrow { IllegalArgumentException("Task with id $taskId not found") }
+        val innloggetIdent = innloggetSaksbehandlerService.getInnloggetIdent()
+        val innloggetName = saksbehandlerService.getNameForIdentDefaultIfNull(
+            navIdent = innloggetIdent
+        )
+        task.apply {
+            dateHandled = LocalDateTime.now()
+            handledBy = innloggetIdent
+            handledByName = innloggetName
+            comment = inputComment
+        }
+    }
+}
