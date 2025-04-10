cde0e3a25a2b6f49ea8ba0369a92071787e9924c
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-04-10 06:33:09 +0200

    Remove non null constraint from varsletBehandlingstidHistorikkMottaker table.

--- src/main/resources/db/migration/V202__name_in_fullmektighistorikk.sql
+++ src/main/resources/db/migration/V202__name_in_fullmektighistorikk.sql
@@ -1,2 +1,8 @@
 ALTER TABLE klage.fullmektighistorikk
     ADD COLUMN fullmektig_name TEXT;
+
+ALTER TABLE klage.varslet_behandlingstid_historikk_mottaker_info
+    ALTER COLUMN varslet_behandlingstid_historikk_mottaker_type DROP NOT NULL;
+
+ALTER TABLE klage.varslet_behandlingstid_historikk_mottaker_info
+    ALTER COLUMN varslet_behandlingstid_historikk_mottaker_value DROP NOT NULL;
