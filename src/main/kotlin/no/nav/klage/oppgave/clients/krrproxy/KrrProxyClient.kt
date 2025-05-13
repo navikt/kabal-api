2f289a678d340b4f300071c17350e18aa8c8c0f5
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-05-13 12:51:14 +0200

    Content type header.

--- src/main/kotlin/no/nav/klage/oppgave/clients/krrproxy/KrrProxyClient.kt
+++ src/main/kotlin/no/nav/klage/oppgave/clients/krrproxy/KrrProxyClient.kt
@@ -6,6 +6,7 @@ import no.nav.klage.oppgave.util.getSecureLogger
 import no.nav.klage.oppgave.util.logErrorResponse
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatusCode
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
@@ -52,6 +53,7 @@ class KrrProxyClient(
                 HttpHeaders.AUTHORIZATION,
                 "Bearer $token"
             )
+            .contentType(MediaType.APPLICATION_JSON)
             .bodyValue(KrrProxyRequest(personidenter = listOf(fnr)))
             .retrieve()
             .onStatus(HttpStatusCode::isError) { response ->
