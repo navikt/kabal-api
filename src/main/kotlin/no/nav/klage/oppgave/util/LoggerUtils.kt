a11c6c1379238ade390d726e835084fc51afd084
Andreas Jonsson <andreas.jonsson@nav.no>
2025-08-29 10:58:43 +0200

    Keep the same exception, just log.

--- src/main/kotlin/no/nav/klage/oppgave/util/LoggerUtils.kt
+++ src/main/kotlin/no/nav/klage/oppgave/util/LoggerUtils.kt
@@ -3,6 +3,7 @@ package no.nav.klage.oppgave.util
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.web.reactive.function.client.ClientResponse
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import reactor.core.publisher.Mono
 import java.util.*
 
@@ -17,7 +18,12 @@ fun getSecureLogger(): Logger = LoggerFactory.getLogger("team-logs")
 
 fun getAuditLogger(): Logger = LoggerFactory.getLogger("audit")
 
-fun logKlagebehandlingMethodDetails(methodName: String, innloggetIdent: String, klagebehandlingId: UUID, logger: Logger) {
+fun logKlagebehandlingMethodDetails(
+    methodName: String,
+    innloggetIdent: String,
+    klagebehandlingId: UUID,
+    logger: Logger
+) {
     logger.debug(
         "{} is requested by ident {} for klagebehandlingId {}",
         methodName,
@@ -43,11 +49,15 @@ fun logMethodDetails(methodName: String, innloggetIdent: String, logger: Logger)
     )
 }
 
-fun logErrorResponse(response: ClientResponse, functionName: String, classLogger: Logger): Mono<RuntimeException> {
-    return response.bodyToMono(String::class.java).map {
-        val errorString = "Got ${response.statusCode()} when requesting $functionName"
-        classLogger.error("$errorString. See team-logs for more details.")
-        getTeamLogger().error("$errorString - response body: '$it'")
-        RuntimeException(errorString)
-    }
+fun logErrorResponse(
+    response: ClientResponse,
+    functionName: String,
+    classLogger: Logger,
+): Mono<WebClientResponseException> {
+    val errorString = "Got ${response.statusCode()} when requesting $functionName"
+    return response.createException()
+        .doOnNext { ex ->
+            classLogger.error("$errorString. See team-logs for more details.")
+            getTeamLogger().error("$errorString - response body: '${ex.responseBodyAsString}'")
+        }
 }
