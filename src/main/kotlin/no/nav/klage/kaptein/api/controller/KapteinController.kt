f98bd9c6d46e9225bc10ab349f2f786bc3a5fd98
Andreas Jonsson <andreas.jonsson@nav.no>
2025-09-09 13:06:45 +0200

    Swagger doc to Kaptein endpoints.

--- src/main/kotlin/no/nav/klage/kaptein/api/controller/KapteinController.kt
+++ src/main/kotlin/no/nav/klage/kaptein/api/controller/KapteinController.kt
@@ -1,10 +1,16 @@
 package no.nav.klage.kaptein.api.controller
 
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.servlet.http.HttpServletResponse
+import no.nav.klage.kaptein.api.view.AnonymousBehandlingListView
+import no.nav.klage.kaptein.api.view.AnonymousBehandlingView
 import no.nav.klage.kaptein.service.KapteinService
 import no.nav.klage.oppgave.config.SecurityConfiguration
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -17,6 +23,15 @@ class KapteinController(
     private val kapteinService: KapteinService,
 ) {
 
+    @ApiResponse(
+        responseCode = "200",
+        content = [
+            Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = AnonymousBehandlingListView::class)
+            )
+        ]
+    )
     @GetMapping("/behandlinger")
     fun getBehandlinger(
         httpServletResponse: HttpServletResponse,
@@ -24,6 +39,15 @@ class KapteinController(
         kapteinService.writeBehandlingerStreamedToOutputStream(httpServletResponse)
     }
 
+    @ApiResponse(
+        responseCode = "200", description = "NDJSON stream",
+        content = [
+            Content(
+                mediaType = MediaType.APPLICATION_NDJSON_VALUE,
+                schema = Schema(implementation = AnonymousBehandlingView::class)
+            )
+        ]
+    )
     @GetMapping("/behandlinger-stream")
     fun getBehandlingerStream(
         httpServletResponse: HttpServletResponse,
