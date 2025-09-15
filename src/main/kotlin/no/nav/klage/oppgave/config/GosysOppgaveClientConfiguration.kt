a74a74204c38b316d7ab81c32dbbd94eeecfcb62
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-09-15 14:20:07 +0200

    Added missing header to GosysOppgave client.

--- src/main/kotlin/no/nav/klage/oppgave/config/GosysOppgaveClientConfiguration.kt
+++ src/main/kotlin/no/nav/klage/oppgave/config/GosysOppgaveClientConfiguration.kt
@@ -1,9 +1,13 @@
 package no.nav.klage.oppgave.config
 
+import io.opentelemetry.api.trace.Span
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.ClientRequest
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 
 @Configuration
 class GosysOppgaveClientConfiguration(private val webClientBuilder: WebClient.Builder) {
@@ -14,7 +18,18 @@ class GosysOppgaveClientConfiguration(private val webClientBuilder: WebClient.Bu
     fun gosysOppgaveWebClient(): WebClient {
         return webClientBuilder
             .baseUrl(gosysOppgaveBaseURL)
+            .filter(
+                ExchangeFilterFunction.ofRequestProcessor { request ->
+                    val traceId = Span.current().spanContext.traceId
+                    Mono.just(
+                        ClientRequest.from(request)
+                            .headers { headers ->
+                                headers["X-Correlation-ID"] = traceId
+                            }
+                            .build()
+                    )
+                }
+            )
             .build()
     }
-
 }
