8f0914c191054b1d43df34fae2dbd6dd47af2dee
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-09-16 11:43:13 +0200

    Missing headers towards GosysOppgave.

--- src/main/kotlin/no/nav/klage/oppgave/config/GosysOppgaveClientConfiguration.kt
+++ src/main/kotlin/no/nav/klage/oppgave/config/GosysOppgaveClientConfiguration.kt
@@ -5,6 +5,8 @@ import no.nav.klage.oppgave.util.getLogger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.ClientRequest
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
@@ -37,6 +39,8 @@ class GosysOppgaveClientConfiguration(private val webClientBuilder: WebClient.Bu
                     )
                 }
             )
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
             .build()
     }
 }
