5605132ba81efac811f6963d0ba89ca75f5f0e8f
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-10-06 16:02:37 +0200

    DB adjustments.
    Co-authored-by: Andreas Jonsson <andreas.jonsson@nav.no>

--- src/main/resources/db/migration/V219__move_mottak_dokument_to_behandling.sql
+++ src/main/resources/db/migration/V219__move_mottak_dokument_to_behandling.sql
@@ -9,16 +9,16 @@ UPDATE klage.behandling b
 SET initiating_system = (SELECT sent_from FROM klage.mottak WHERE b.mottak_id = id)
 WHERE mottak_id IS NOT NULL;
 
----
-
 ALTER TABLE klage.mottak_dokument
-    ADD COLUMN behandling_id UUID REFERENCES klage.behandling (id);
-
-ALTER TABLE klage.behandling
-    ADD COLUMN sent_from TEXT;
+    ADD COLUMN behandling_id UUID;
 
 UPDATE klage.mottak_dokument md
 SET behandling_id = (SELECT b.id FROM klage.behandling b WHERE b.mottak_id = md.mottak_id);
+
+ALTER TABLE klage.mottak_dokument
+    ADD CONSTRAINT fk_mottak_dokument_behandling
+        FOREIGN KEY (behandling_id)
+            REFERENCES klage.behandling (id);
 
 ALTER TABLE klage.mottak_dokument
     DROP COLUMN mottak_id;
