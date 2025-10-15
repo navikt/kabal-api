34dc9181d756b4453dd238fe16dd1397045bd359
Andreas Jonsson <andreas.jonsson@nav.no>
2025-10-15 11:26:08 +0200

    Include info about initiatingSystem to Kaptein.

--- src/main/kotlin/no/nav/klage/kaptein/service/KapteinService.kt
+++ src/main/kotlin/no/nav/klage/kaptein/service/KapteinService.kt
@@ -130,6 +130,7 @@ class KapteinService(
             tildeltEnhet = behandling.tildeling?.enhet,
             previousTildeltEnhet = null,
             previousRegistreringshjemmelIdList = null,
+            initiatingSystem = behandling.initiatingSystem,
         )
     }
 
@@ -162,6 +163,7 @@ class KapteinService(
             tildeltEnhet = behandling.tildeling?.enhet,
             previousTildeltEnhet = null,
             previousRegistreringshjemmelIdList = null,
+            initiatingSystem = behandling.initiatingSystem,
         )
     }
 
@@ -194,6 +196,7 @@ class KapteinService(
             tildeltEnhet = behandling.tildeling?.enhet,
             previousTildeltEnhet = null,
             previousRegistreringshjemmelIdList = null,
+            initiatingSystem = behandling.initiatingSystem,
         )
     }
 
@@ -227,6 +230,7 @@ class KapteinService(
             tildeltEnhet = behandling.tildeling?.enhet,
             previousTildeltEnhet = previousBehandling?.tildeling?.enhet,
             previousRegistreringshjemmelIdList = previousBehandling?.registreringshjemler?.map { it.id },
+            initiatingSystem = behandling.initiatingSystem,
         )
     }
 
@@ -259,6 +263,7 @@ class KapteinService(
             tildeltEnhet = behandling.tildeling?.enhet,
             previousTildeltEnhet = null,
             previousRegistreringshjemmelIdList = null,
+            initiatingSystem = behandling.initiatingSystem,
         )
     }
 
@@ -291,6 +296,7 @@ class KapteinService(
             tildeltEnhet = behandling.tildeling?.enhet,
             previousTildeltEnhet = null,
             previousRegistreringshjemmelIdList = null,
+            initiatingSystem = behandling.initiatingSystem,
         )
     }
 
@@ -324,6 +330,7 @@ class KapteinService(
             tildeltEnhet = behandling.tildeling?.enhet,
             previousTildeltEnhet = previousBehandling.tildeling?.enhet,
             previousRegistreringshjemmelIdList = previousBehandling.registreringshjemler.map { it.id },
+            initiatingSystem = behandling.initiatingSystem,
         )
     }
 
