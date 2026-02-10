a5b91d7070e379e8fea4a938d8c45f3a5ad8e52c
Andreas Jonsson <andreas.jonsson@nav.no>
2026-02-10 13:13:02 +0100

    No need to index saksdokumenter in kabal-search or Kaptein.

--- src/main/kotlin/no/nav/klage/oppgave/eventlisteners/SendBehandlingEndretToKafkaEventListener.kt
+++ src/main/kotlin/no/nav/klage/oppgave/eventlisteners/SendBehandlingEndretToKafkaEventListener.kt
@@ -2,6 +2,7 @@ package no.nav.klage.oppgave.eventlisteners
 
 import no.nav.klage.kaptein.service.KapteinService
 import no.nav.klage.oppgave.domain.events.BehandlingChangedEvent
+import no.nav.klage.oppgave.domain.events.BehandlingChangedEvent.Felt
 import no.nav.klage.oppgave.repositories.BehandlingRepository
 import no.nav.klage.oppgave.service.BehandlingEndretKafkaProducer
 import no.nav.klage.oppgave.util.getLogger
@@ -29,8 +30,16 @@ class SendBehandlingEndretToKafkaEventListener(
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     fun indexKlagebehandling(behandlingChangedEvent: BehandlingChangedEvent) {
         logger.debug("Received BehandlingEndretEvent for behandlingId {}", behandlingChangedEvent.behandling.id)
-        //full fetch to make sure all collections are loaded
+
+        //Ignore events where only saksdokumenter have changed, as this is not relevant for Kaptein or kabal-search,
+        //and would cause too many events to be sent when documents are added or removed.
+        if (behandlingChangedEvent.changeList.size == 1 && behandlingChangedEvent.changeList.first().felt == Felt.SAKSDOKUMENT) {
+            return
+        }
+
+        //Full fetch to make sure all collections are loaded
         val behandling = behandlingRepository.findByIdEager(behandlingChangedEvent.behandling.id)
+
         try {
             behandlingEndretKafkaProducer.sendBehandlingEndret(behandling)
         } catch (e: Exception) {
