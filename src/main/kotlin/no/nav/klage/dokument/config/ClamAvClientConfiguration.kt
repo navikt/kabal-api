66e6e6b617108107c236bebbcff7ce60fb478ca7
Andreas Jonsson <andreas.jonsson@nav.no>
2026-04-20 15:27:56 +0200

    Better handling of large files for virus check. Use new API.

--- src/main/kotlin/no/nav/klage/dokument/config/ClamAvClientConfiguration.kt
+++ src/main/kotlin/no/nav/klage/dokument/config/ClamAvClientConfiguration.kt
@@ -1,5 +1,6 @@
 package no.nav.klage.dokument.config
 
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -7,14 +8,24 @@ import org.springframework.web.reactive.function.client.WebClient
 
 
 @Configuration
-class ClamAvClientConfiguration(private val webClientBuilder: WebClient.Builder) {
+class ClamAvClientConfiguration(
+    @Qualifier("webClientBuilder") private val webClientBuilder: WebClient.Builder,
+    @Qualifier("clamAvLargeFileWebClientBuilder") private val clamAvLargeFileWebClientBuilder: WebClient.Builder,
+) {
 
-    @Value("\${CLAM_AV_URL}")
+    @Value($$"${CLAM_AV_URL}")
     private lateinit var url: String
 
     @Bean
     fun clamAvWebClient(): WebClient {
         return webClientBuilder
+            .baseUrl(url)
+            .build()
+    }
+
+    @Bean
+    fun clamAvLargeFileWebClient(): WebClient {
+        return clamAvLargeFileWebClientBuilder
             .baseUrl(url)
             .build()
     }
