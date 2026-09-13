0325efd81b823f37ab60b9f339fc4fa309c21de5
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-09-13 15:55:07 +0200

    Remove unused db in dev, grant privileges on sequence to user.

---
+++ src/main/resources/db/migration/V245__Grant_more_access_to_postgres_user.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS "kabal-smart-editor-api";
+
+DO
+$$
+    BEGIN
+        IF EXISTS
+            (SELECT 1 from pg_roles where rolname = 'postgres')
+        THEN
+            GRANT SELECT ON ALL SEQUENCES IN SCHEMA flyway_history_schema TO postgres;
+            GRANT SELECT ON ALL SEQUENCES IN SCHEMA klage TO postgres;
+            ALTER DEFAULT PRIVILEGES IN SCHEMA flyway_history_schema GRANT SELECT ON SEQUENCES TO postgres;
+            ALTER DEFAULT PRIVILEGES IN SCHEMA klage GRANT SELECT ON SEQUENCES TO postgres;
+        END IF;
+    END
+$$;
